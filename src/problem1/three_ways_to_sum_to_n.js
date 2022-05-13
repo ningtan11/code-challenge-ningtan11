62fdf173548a9913fbd0950f516d3353fe9e7457
Ng Wen Hao Dennis <36025582+Denniszedead@users.noreply.github.com>
2022-05-13 15:45:29 +0800

    Remove test method at three_ways_to_sum_to_n.js

--- src/problem1/three_ways_to_sum_to_n.js
+++ src/problem1/three_ways_to_sum_to_n.js
@@ -31,18 +31,3 @@ var sum_to_n_c = function(n) {
     const result = noOfTerm * (firstTerm + lastTerm) / 2;
     return result
 };
-
-for (var i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
-    var a = sum_to_n_a(i);
-    var b = sum_to_n_b(i);
-    var c = sum_to_n_c(i)
-
-    console.log("i: " + i);
-    console.log("a: " + a);
-    console.log("b: " + b);
-    console.log("c: " + c);
-
-    if (a != b || a != c || b != c) {
-        break;
-    }
-}
