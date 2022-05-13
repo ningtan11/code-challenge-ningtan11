d56537ada804a332f870ab092c8c354948b0b851
denniszedead <dennisng2255@gmail.com>
2022-05-13 15:13:06 +0800

    Update assumption of n

--- src/problem1/three_ways_to_sum_to_n.js
+++ src/problem1/three_ways_to_sum_to_n.js
@@ -1,10 +1,14 @@
+// We assume that n > 0
+const firstTerm = 1;
+const difference = 1;
+
 var sum_to_n_a = function(n) {
     // your code here
-    if (n < 0) {
+    if (n <= 0) {
         return 0;
     }
-    const noOfTerms = n + 1;
-    const result = (noOfTerms / 2) * (noOfTerms - 1);
+    const noOfTerms = n;
+    const result = (noOfTerms / 2) * ((2 * firstTerm) + (noOfTerms - 1) * difference);
     return result;
 };
 
@@ -22,8 +26,23 @@ var sum_to_n_c = function(n) {
     if (n < 0) {
         return 0;
     }
-    const noOfTerm = n + 1;
+    const noOfTerm = n;
     const lastTerm = n;
-    const result = noOfTerm * (0 + lastTerm) / 2;
+    const result = noOfTerm * (firstTerm + lastTerm) / 2;
     return result
 };
+
+for (var i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
+    var a = sum_to_n_a(i);
+    var b = sum_to_n_b(i);
+    var c = sum_to_n_c(i)
+
+    console.log("i: " + i);
+    console.log("a: " + a);
+    console.log("b: " + b);
+    console.log("c: " + c);
+
+    if (a != b || a != c || b != c) {
+        break;
+    }
+}
